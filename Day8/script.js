07283793f2948a9c4fe8fe9677223c09f10a2ba8
Samiksha Bhore <samikshabhore7636@gmail.com>
2025-12-22 17:47:42 +0530

    Added Day8 learnt variables datatypes conditionals loops

---
+++ Day8/script.js
@@ -0,0 +1,12 @@
+let userName = "Samiksha";
+const pi = 3.14;
+var age = 5;
+
+if (age === 5) {
+    console.log("age is 5");
+}
+
+let i;
+for (i = 0; i <= 3; i++) {
+    console.log("Hello " + userName + "." + " Good Morning " + userName)
+}
